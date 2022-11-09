12f20f4713fb436f6db867b0d0fd6ae267bfec66
choi217 <1206294353@qq.com>
2022-11-09 20:21:10 +0800

    app

---
+++ app/src/main/java/com/example/myapp/teacher_info_show.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class teacher_info_show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_info_show);
+    }
+}
