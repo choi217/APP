dc3a9e20bcb7adc9ce5df6368e97e416eb716d2c
forever-learner <2360561713@qq.com>
2022-11-09 19:39:10 +0800

    first-commit

---
+++ myapp/src/main/java/com/example/myapp/student_info_show.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class student_info_show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_info_show);
+    }
+}
