2cbde00eed70a8f4e17984ea6667e726a5e05966
forever-learner <2360561713@qq.com>
2022-11-12 14:21:50 +0800

    tryzzh-branch

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//这是一行测试注释
 public class MainActivity extends AppCompatActivity {
     //声明控件
     private Button m_btn_signup;
