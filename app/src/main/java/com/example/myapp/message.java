c40f2f7b14c3466490432879a2a73846e02097c4
choi217 <1206294353@qq.com>
2023-01-11 21:04:32 +0800

    聊天

--- app/src/main/java/com/example/myapp/message.java
+++ app/src/main/java/com/example/myapp/message.java
@@ -13,13 +13,26 @@ import android.view.View;
 import android.widget.Button;
 
 public class message extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.activity_message,container,false);
     }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button btn;
+        btn = getActivity().findViewById(R.id.a);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), talk.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
 
 
